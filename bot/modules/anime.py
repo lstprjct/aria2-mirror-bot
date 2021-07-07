f9807626998bde15452538fe76ccb229d0f9243e
lstprjct <84672184+lstprjct@users.noreply.github.com>
2021-07-07 22:38:04 +0600

    Update anime.py

--- bot/modules/anime.py
+++ bot/modules/anime.py
@@ -256,7 +256,7 @@ def weebhelp(update, context):
 • `/chr`*:* search character
 • `/mng`*:* search manga
 '''
-    update.effective_message.reply_photo("https://telegra.ph/file/db03910496f06094f1f7a.jpg", help_string, parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_photo("https://telegra.ph/file/9b6653d00c49b1e51abfc.jpg", help_string, parse_mode=ParseMode.MARKDOWN)
 
 
 ANIME_HANDLER = CommandHandler("al", anime)
